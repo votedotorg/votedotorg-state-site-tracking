8294d800b7b08f8f6fc2d322711ee04e02fa6a33
Adeola Awoyemi <adeola.awoyemi@verizonmedia.com>
2020-09-17 15:49:57 -0700

    saving data to db

--- bin/import-csv.js
+++ bin/import-csv.js
@@ -8,7 +8,8 @@ const rawSourceFile = 'misc/2020-09-16-vdo-state-urls.csv';
 const connetionString = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`;
 const parser = csvParse({ columns: true });
 
-let output = [];
+const ScrapeItemSchema = require('../src/database/scrape-item');
+const ScrapeItem = mongoose.model('ScrapeItem', ScrapeItemSchema);
 
 mongoose.connect(connetionString, {useNewUrlParser: true});
 const db = mongoose.connection;
@@ -22,8 +23,15 @@ db.once('open', function() {
 fs.createReadStream(`${__dirname}/../${rawSourceFile}`)
     .pipe(parser)
     .on('data', row => {
-        console.log('ROW:', row);
-        output.push(row);
+        // console.log('ROW:', row);
+        const item = new ScrapeItem({
+            state: row['State abbr'],
+            url: row['Absentee Info URL'],
+        });
+        item.save((err, currentItem) => {
+            if (err) return console.error(err);
+            console.info(chalk.greenBright('SAVED:', currentItem));
+        });
     })
     .on('error', err => {
         console.error(err.message);

2aedb4dc755d0c2432eda59574c9985fbe7e45ed
Adonis Che Allen <che@verizonmedia.com>
2020-09-17 13:42:06 -0700

    Only pdfs should be in a list.

--- src/database/scrape-attempt.js
+++ src/database/scrape-attempt.js
@@ -3,7 +3,9 @@ const { Schema } = require('mongoose');
 module.exports = new Schema({
     type: String,
     timestamp: Date,
-    urls: [new Schema({ url: String, hash: String })],  // url: html link or reference pdf file
+    url: String,
+    hash: String,
+    pdfs: [new Schema({ url: String, hash: String })],    // could have multiple pdf links on page
     content: String,
     enableScrape: Boolean,
     lastChangeDate: Date,

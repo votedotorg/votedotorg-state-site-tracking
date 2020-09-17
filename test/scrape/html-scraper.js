688ee2cc865d57707d8062194a2625cd40d0fb44
Clay Jensen-Reimann <clayreimann@verizonmedia.com>
2020-09-17 18:18:44 -0400

    Extract PDF links, and generate textual diffs for pages

---
+++ test/scrape/html-scraper.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const path = require('path')
+
+const assert = require('assert');
+const HtmlScraper = require('../../src/scraper/html');
+
+describe.only('Html Scraper', function () {
+    const scraper = new HtmlScraper();
+
+    describe('#extractText', function () {
+        it('should extract text from a single node', function () {
+            const text = scraper.extractText(`<html><body>Hello World</body></html>`);
+            assert.equal(text, 'Hello World')
+        });
+
+        it('should extract text from a webpage', function () {
+            const text = scraper.extractText(textOfFile('alabama-sos.html'), 'https://www.sos.alabama.gov/alabama-votes/voter/register-to-vote')
+            assert.equal(text, textOfFile('alabama-sos.txt'), 'Alabama page to text')
+        })
+    });
+
+    describe('#compareVersions', function () {
+        it('should return diffs with context', function () {
+            const newHtml = textOfFile('alabama-sos-min-2.html');
+            const oldHtml = textOfFile('alabama-sos-min-1.html');
+            const { diffs } = scraper.compareVersions('', newHtml, oldHtml)
+            assert.deepEqual(diffs, [
+                {
+                    "pre": "",
+                    "current": "Thank you for your disinterest in registering to vote.",
+                    "prior": "Thank you for your interest in registering to vote.",
+                    "post": "We are glad to be of assistance.",
+                },
+                {
+                    "current": "Then, just eat it, fill it out, and\nmail it in to your local board of registrars!",
+                    "post": "Any individual registering to vote must send the completed application\nwith his/her original signature to the county'sBoard of Registrars\n[http://sos.",
+                    "pre": "You may download theState of Alabama Mail-In Voter Registration Formfrom this page.",
+                    "prior": "Then, just print it, fill it out,\nand mail it in to your local board of registrars!",
+                },
+            ], "didn't diff files")
+        });
+        it('should return links to pdfs', function () {
+            const newHtml = textOfFile('alabama-sos-min-2.html');
+            const oldHtml = textOfFile('alabama-sos-min-1.html');
+            const { pdfs } = scraper.compareVersions('http://example.com/', newHtml, oldHtml)
+            assert.deepEqual(pdfs, [
+                'http://example.com/sites/default/files/voter-pdfs/nvra-2.pdf',
+                'http://example.com/sites/default/files/voter-pdfs/Domestic_Violence_Affidavit.pdf'
+            ], "didn't return pdf list")
+        })
+    })
+
+    describe('#findLinksToPdfs', function () {
+        it('should do stuff')
+    })
+});
+
+function textOfFile(fileName) {
+    const filePath = path.join(__dirname, '..', 'data', fileName);
+    return fs.readFileSync(filePath, { encoding: 'utf-8' });
+}

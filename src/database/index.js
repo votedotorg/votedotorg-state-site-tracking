05c728fc0c6402658d3fcbc81b8a3e4450e5d5af
Adonis Che Allen <che@verizonmedia.com>
2020-09-18 08:50:05 -0700

    Moved Mongo DB url into separate config.

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,19 @@
-//
+const mongoose = require('mongoose');
+
+const { MONGODB_URL: mongoDBUrl } = process.env.NODE_ENV === 'production' ? process.env : require('./config');
+
+// TODO: this makes the tests hang forever because we don't have a way to shut the connection down
+// mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true });
+mongoose.connect(mongoDBUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Get the default connection
+const connection = mongoose.connection;
+connection.once('open', function () {
+  console.log('MongoDB database connection established successfully');
+});
+connection.on('error', function () {
+  console.log('Error connecting to MongoDB database');
+});
 
 async function loadItemsToScrape() {
   const ScrapeItem = require('./scrape-item');
@@ -22,10 +37,17 @@ async function getMostRecentAttempt(forUrl) {
 }
 
 async function saveScrapeAttempt(forUrl, didChange, timestamp, type, hash, content) {
+  const ScrapeAttempt = require('./scrape-attempt');
   // always record that an attempt occurred
   // only record results if didChange is true
-  const success = true;
-  return Promise.resolve(success);
+  return new ScrapeAttempt({
+    scrapeItemId: Schema.ObjectId,
+    scrapeStartDate: timestamp,
+    scrapeEndDate: Date.now(),
+    runJobId: Schema.ObjectId, // updates for each scrape run
+    status: 'success',
+    errorInfo: '',
+  }).save();
 }
 
 async function markUrlDefunct(url, timestamp) {
--- src/main.js
+++ src/main.js
@@ -1,27 +1,10 @@
 const api = require('./api');
-const mongoose = require('mongoose');
 
 const port = process.env.PORT || 3000;
 api.listen(port, () => {
   console.log(`Server started on ${port}`);
-
-  // TODO: this makes the tests hang forever because we don't have a way to shut the connection down
-  // mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true });
-  mongoose.connect(
-    'mongodb+srv://vote-org-build:4Mj2ExE0ip4N6QaJ@vote-org.k63d9.mongodb.net/voteorg?retryWrites=true&w=majority',
-    { useNewUrlParser: true, useUnifiedTopology: true },
-  );
-
-  //Get the default connection
-  const connection = mongoose.connection;
-  connection.once('open', function () {
-    console.log('MongoDB database connection established successfully');
-  });
-  connection.on('error', function () {
-    console.log('Error connecting to MongoDB database');
-  });
-
-  const Scheduler = require('./scheduler');
-  const s = new Scheduler(60 * 60 * 1000);
-  s.start();
 });
+
+const Scheduler = require('./scheduler');
+const s = new Scheduler(60 * 60 * 1000);
+s.start();
